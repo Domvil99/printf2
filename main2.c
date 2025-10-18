303c7238c1b80e089e1a0124d1f902245b1b1d51
Domvil99 <santiospina33@gmail.com>
2025-10-18 20:02:55 +0200

    main$

---
+++ main2.c
@@ -0,0 +1,8 @@
+int main(void)
+{
+    printf("int: min = %d, max = %d\n", INT_MIN, INT_MAX);
+    printf("unsigned int: max = %u\n", UINT_MAX);
+    printf("long long: min = %lld, max = %lld\n", LLONG_MIN, LLONG_MAX);
+    printf("unsigned long long: max = %llu\n", ULLONG_MAX);
+    return 0;
+}
